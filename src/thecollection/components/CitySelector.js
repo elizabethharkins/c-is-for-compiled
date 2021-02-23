350956eae36a949ae50e0eea2041b1f126a1b161
elizabethharkins <elizabeth.lidstrom@gmail.com>
2021-02-22 21:43:49 -0500

    prep for useEffect hook

--- src/thecollection/components/CitySelector.js
+++ src/thecollection/components/CitySelector.js
@@ -3,33 +3,18 @@
 import React, { useState } from "react";
 import { Row, Col, FormControl, Button } from "react-bootstrap";
 import styled from "styled-components";
-const SelectorWrapper = styled.div`
+const SelectorStyles = styled.div`
 	.row {
 		justify-content: center;
 	  	margin: 15px 0;
 	}
 `;
 
-const CitySelector = () => {
+const CitySelector = ({ onSearch }) => {
   	const [city, setCity] = useState('');
-  	const [results, setResults] = useState(null);
-
-  	const onSearch = () => {
-  		fetch(`${process.env.REACT_APP_API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
-  			.then((response) => response.json())
-  			// .then((result) => console.log(result));
-  			.then((results) => setResults(results));
-  			console.log(results);
-  	};
-
-  	const onKeyDown = (e) => {
-  		if (e.keyCode === 13) {
-  			onSearch();
-  		}
-  	}
 
   	return (
-  		<SelectorWrapper>
+  		<SelectorStyles>
 			<Row>
 				<Col>
 			  		<h1>Search your city</h1>
@@ -42,17 +27,16 @@ const CitySelector = () => {
 						placeholder="Enter city"
 						onChange={ (e) => setCity(e.target.value) }
 						value={ city }
-						onKeyDown={ onKeyDown }
 			  		/>
 				</Col>
 		  	</Row>
 
 		  	<Row>
 				<Col>
-			  		<Button onClick={onSearch}>Check Weather</Button>
+			  		<Button onClick={() => onSearch(city)}>Check Weather</Button>
 				</Col>
 		  	</Row>
-		</SelectorWrapper>
+		</SelectorStyles>
   	);
 };
 
