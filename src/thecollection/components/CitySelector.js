1d9168f87842e73cf4a1638dd41d86118129cc75
elizabethharkins <elizabeth.lidstrom@gmail.com>
2021-02-22 20:29:31 -0500

    set results, useState

--- src/thecollection/components/CitySelector.js
+++ src/thecollection/components/CitySelector.js
@@ -12,12 +12,21 @@ const SelectorWrapper = styled.div`
 
 const CitySelector = () => {
   	const [city, setCity] = useState('');
+  	const [results, setResults] = useState(null);
 
   	const onSearch = () => {
   		fetch(`${process.env.REACT_APP_API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
   			.then((response) => response.json())
-  			.then((result) => console.log(result));
+  			// .then((result) => console.log(result));
+  			.then((results) => setResults(results));
+  			console.log(results);
   	};
+
+  	const onKeyDown = (e) => {
+  		if (e.keyCode === 13) {
+  			onSearch();
+  		}
+  	}
 
   	return (
   		<SelectorWrapper>
@@ -33,6 +42,7 @@ const CitySelector = () => {
 						placeholder="Enter city"
 						onChange={ (e) => setCity(e.target.value) }
 						value={ city }
+						onKeyDown={ onKeyDown }
 			  		/>
 				</Col>
 		  	</Row>
