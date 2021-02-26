8cb1a1febb5063bf035ee15dd72c685236a1ce03
elizabethharkins <elizabeth.lidstrom@gmail.com>
2021-02-25 19:37:08 -0500

    zine component, estab

--- src/about/About.js
+++ src/about/About.js
@@ -8,13 +8,31 @@ import { Quote } from "./components/Quote";
 import { Synthwave } from "./components/Synthwave";
 import { Drawing } from "./components/Drawing";
 import { Invite } from "./components/Invite";
-import "./About.css";
+import styled from "styled-components";
+const ZineStyles = styled.div`
+	background: #e5e5e5;
+	display: grid;
+	grid-template-rows: 130px 2fr 1fr;
+	grid-template-columns: 1.5fr 1fr 1fr 1.5fr;
+	height: 800px;
+	max-width: 80em;
+	outline: 6px solid #121219;
+	width: 95vw;
+	margin: 5% auto auto;
+
+	@media only screen and (max-width: 73em) {
+		grid-template-rows: 120px 4fr 3fr 3fr 2fr 0.5fr;/*130px 2fr 1fr*/
+		grid-template-columns: 95vw; /*1.5fr 1fr 1fr 1.5fr*/
+		height: 1800px;
+		width: 95vw;
+	}
+`;
 
 
 export const About = () => {
 
 	return (
-		<div className="magazine">
+		<ZineStyles>
 			<Title />
 			<Primary />
 			<Supplementary />
@@ -22,7 +40,7 @@ export const About = () => {
 			<Synthwave />
 			<Drawing />
 			<Invite />
-		</div>
+		</ZineStyles>
 	);
 
 }
